4d2a4b3738160c22a30af3f5cbb28d1eed3e4b92
Matt Millar <matthew.millar@madetech.com>
2020-12-07 21:47:02 +0000

    Day 3 Part 2

--- lib/day_three.ex
+++ lib/day_three.ex
@@ -1,9 +1,17 @@
 defmodule AOC.DayThree do
-  def num_trees(map) do
+  def num_trees_product(map, slopes) do
+    slopes
+    |> Enum.reduce(1, fn slope, acc ->
+      acc * num_trees(map, slope)
+    end)
+  end
+
+  def num_trees(map, slope) do
     map
+    |> Enum.take_every(slope.y)
     |> Enum.with_index()
     |> Enum.reduce(0, fn {line, index}, acc ->
-      case String.at(line, rem(index * 3, String.length(line))) do
+      case String.at(line, rem(index * slope.x, String.length(line))) do
         "#" when index > 0 ->
           acc + 1
 
--- test/day_three_test.exs
+++ test/day_three_test.exs
@@ -3,9 +3,10 @@ defmodule AOC.DayThreeTest do
 
   setup_all do
     [
-      sample_set:
-        String.split(
-          "..##.........##.........##.........##.........##.........##.......
+      sample_set: %{
+        map:
+          String.split(
+            "..##.........##.........##.........##.........##.........##.......
         #...#...#..#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..
         .#....#..#..#....#..#..#....#..#..#....#..#..#....#..#..#....#..#.
         ..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#
@@ -16,18 +17,35 @@ defmodule AOC.DayThreeTest do
         #.##...#...#.##...#...#.##...#...#.##...#...#.##...#...#.##...#...
         #...##....##...##....##...##....##...##....##...##....##...##....#
         .#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#",
-          "\n"
-        )
-        |> Enum.map(&String.trim/1),
-      problem_set: AOC.Helpers.get_problem_set(3)
+            "\n"
+          )
+          |> Enum.map(&String.trim/1),
+        slopes: [%{x: 1, y: 1}, %{x: 3, y: 1}, %{x: 5, y: 1}, %{x: 7, y: 1}, %{x: 1, y: 2}]
+      },
+      problem_set: %{
+        map: AOC.Helpers.get_problem_set(3),
+        slopes: [%{x: 1, y: 1}, %{x: 3, y: 1}, %{x: 5, y: 1}, %{x: 7, y: 1}, %{x: 1, y: 2}]
+      }
     ]
   end
 
   test "gets the number of trees in the sample set", %{sample_set: sample_set} do
-    assert AOC.DayThree.num_trees(sample_set) == 7
+    assert AOC.DayThree.num_trees(sample_set.map, %{x: 3, y: 1}) == 7
   end
 
-  test "gets the number of trees in the problem set", %{problem_set: problem_set} do
-    IO.puts(AOC.DayThree.num_trees(problem_set))
+  test "gets the product of the number of trees at different slopes in the sample set", %{
+    sample_set: sample_set
+  } do
+    assert AOC.DayThree.num_trees_product(sample_set.map, sample_set.slopes) == 336
   end
+
+  # test "gets the number of trees in the problem set", %{problem_set: problem_set} do
+  #   IO.puts(AOC.DayThree.num_trees(problem_set.map, %{x: 3, y: 1}))
+  # end
+
+  # test "gets the product of the number of trees at different slopes in the problem set", %{
+  #   problem_set: problem_set
+  # } do
+  #   IO.puts(AOC.DayThree.num_trees_product(problem_set.map, problem_set.slopes))
+  # end
 end
